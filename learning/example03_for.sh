788b4e5f35cf2af427337471670fd9fba38a47ef
Wataru KUNINO <git@bokunimo.net>
2023-11-23 20:56:38 +0900

    引き数なし対応

--- learning/example03_for.sh
+++ learning/example03_for.sh
@@ -2,10 +2,12 @@
 # Example 03 コンピュータお得意の繰り返しfor文
 
 echo "Usage: "${0}" [name]..."          # プログラム名と使い方を表示する
-if [[ ${#} > 0 ]]; then                 # 引数が1個以上の時
-    echo ${@}                           # 入力パラメータを表示する
-fi                                      # 条件文ifの処理の終了
 
-for name in ${@}; do                    # 引数を変数nameへ代入
-    echo "Hello, "${name}"!"            # 文字列Helloと変数nameの内容を表示
-done                                    # forループの続きを繰り返す
+if [[ ${#} == 0 ]]; then                # 取得した引数が0個のとき
+    echo "Hello, World!"                # 文字列 Hello, World! を出力する
+else
+    echo ${@}                           # 入力パラメータを表示する
+    for name in ${@}; do                # 引数を変数nameへ代入
+        echo "Hello, "${name}"!"        # 文字列Helloと変数nameの内容を表示
+    done                                # forループの続きを繰り返す
+fi                                      # 条件文ifの処理の終了
