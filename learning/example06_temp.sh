5d622ba9c44baea7e3eaad075a7dc54b67628753
Wataru KUNINO <git@bokunimo.net>
2023-11-28 20:09:30 +0900

    renamed file

--- learning/example06_temp.sh
+++ learning/example06_temp.sh
@@ -3,8 +3,8 @@
 
 echo "Usage: "${0}                  # プログラム名を表示する
 
-filename="/sys/class/thermal/thermal_zone0/temp" # 温度ファイル
+file="/sys/class/thermal/thermal_zone0/temp" # 温度ファイル
 
-temp=`cat ${filename}`              # ファイルの内容を変数tempに保持
+temp=`cat ${file}`                  # ファイルの内容を変数tempに保持
 temp=$(( $temp / 1000 ))            # 変数tempの値を1000で除算
 echo "Temperature = "${temp}        # 温度を表示する
