cbf5e88cfb9e4d8f9174aae49731494d69c63f72
Wataru KUNINO <git@bokunimo.net>
2023-11-29 20:44:58 +0900

    無限→永久

--- learning/example08_while.sh
+++ learning/example08_while.sh
@@ -4,10 +4,10 @@
 echo "Usage: "${0}                                  # プログラム名の表示
 echo "       Press [Ctrl]+[C] key to exit."         # 終了方法の表示
 
-while true; do                                      # 無限ループ
+while true; do                                      # 永久ループ
     data=`./example06_temp.sh | grep "Temperature"` # 温度データを取得
     array=(echo ${data})                            # データ1行分を配列に
     time=`date "+%Y/%m/%d %T"`                      # 日時を取得
     echo ${time}", "${array[3]}                     # 日時と温度を表示
     sleep 5                                         # 5秒間の待ち時間処理
-done                                                # 無限ループを繰り返す
+done                                                # 永久ループを繰り返す
--- learning/example08_while_pipe.sh
+++ learning/example08_while_pipe.sh
@@ -4,7 +4,7 @@
 echo "Usage: "${0}                                  # プログラム名の表示
 echo "       Press [Ctrl]+[C] key to exit."         # 終了方法の表示
 
-while true; do                                      # 無限ループ
+while true; do                                      # 永久ループ
     ./example06_temp.sh | while read data; do       # 実行結果を標準入力
         array=(echo ${data})                        # データ1行分を配列に
         if [[ ${array[1]} == "Temperature" ]];then  # 温度データのとき
@@ -13,4 +13,4 @@ while true; do                                      # 無限ループ
         fi                                          # 温度データ処理の終了
     done                                            # 次の行の処理に戻る
     sleep 5                                         # 5秒間の待ち時間処理
-done                                                # 無限ループを繰り返す
+done                                                # 永久ループを繰り返す
