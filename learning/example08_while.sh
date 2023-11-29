1ec534737ddea4c9411371bcc148d193799211fa
Wataru KUNINO <git@bokunimo.net>
2023-11-29 20:56:58 +0900

    こっちの方が分かりやすい

--- learning/example08_while.sh
+++ learning/example08_while.sh
@@ -6,8 +6,8 @@ echo "       Press [Ctrl]+[C] key to exit."         # 終了方法の表示
 
 while true; do                                      # 永久ループ
     data=`./example06_temp.sh | grep "Temperature"` # 温度データを取得
-    array=(echo ${data})                            # データ1行分を配列に
+    array=(`echo ${data}`)                          # データ1行分を配列に
     time=`date "+%Y/%m/%d %T"`                      # 日時を取得
-    echo ${time}", "${array[3]}                     # 日時と温度を表示
+    echo ${time}", "${array[2]}                     # 日時と温度を表示
     sleep 5                                         # 5秒間の待ち時間処理
 done                                                # 永久ループを繰り返す
