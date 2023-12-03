926d5ed58f51cde58301d057e671ad2abfaa0c56
Wataru KUNINO <git@bokunimo.net>
2023-12-03 12:40:34 +0000

    コメント

--- learning/example19_led3.sh
+++ learning/example19_led3.sh
@@ -3,7 +3,7 @@
 # 参考文献 raspi-gpio help
 
 echo "Usage: "${0}" <color>"            # プログラム名と使い方を表示する
-echo "       echo <color>|"${0}
+echo "       echo <color>|"${0}         # 標準入力にも対応
 
 gpio_app="raspi-gpio"                   # GPIO制御に標準のraspi-gpioを使用する
 port_R=17                               # 赤色LED用 GPIO ポート番号
@@ -16,18 +16,18 @@ if [[ ${#} == 0 ]]; then                # 取得した引数が0個のとき
     read color                          # 標準入力から色を代入
 else                                    # 引数が存在するとき
     color=${1}                          # 入力パラメータから色を代入
-fi
+fi                                      # if文の終了
 if [[ ${color} -ge 0 && ${color} -le 7 ]]; then # 色が代入されていた時
-    for i in {0..2}; do
-        port=${ports[${i}]}
-        ${gpio_app} set ${port} op # ポート番号portのGPIOを出力に設定
-        b=$(( (color >> i) & 1))
-        echo "GPIO"${port}"="${b}           # ポート番号と変数bの値を表示
-        if [[ ${b} == 0 ]]; then            # b＝0のとき
-            ${gpio_app} set ${port} dl      # GPIOにLレベル(約0V)を出力
-        else                                # b≠0(b=1)のとき
-            ${gpio_app} set ${port} dh      # GPIOにHレベル(約3.3V)を出力
-        fi                                  # if文の終了
-    done
-fi
-exit
+    for i in {0..2}; do                 # RGBの各LED色に対して
+        port=${ports[${i}]}             # ポート番号を取得
+        ${gpio_app} set ${port} op      # ポート番号portのGPIOを出力に設定
+        b=$(( (color >> i) & 1))        # 該当LEDへの出力値を変数bへ
+        echo "GPIO"${port}"="${b}       # ポート番号と変数bの値を表示
+        if [[ ${b} == 0 ]]; then        # b＝0のとき
+            ${gpio_app} set ${port} dl  # GPIOにLレベル(約0V)を出力
+        else                            # b≠0(b=1)のとき
+            ${gpio_app} set ${port} dh  # GPIOにHレベル(約3.3V)を出力
+        fi                              # if文の終了
+    done                                # ループの終了
+fi                                      # if文の終了
+exit                                    # プログラムの終了
