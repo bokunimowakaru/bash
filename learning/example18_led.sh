3c0503cbe3aa51d12ee271e63eb22958b26032c8
Wataru KUNINO <git@bokunimo.net>
2024-04-20 10:09:56 +0900

    表示有効化

--- learning/example18_led.sh
+++ learning/example18_led.sh
@@ -16,7 +16,7 @@ b=0                                     # GPIO 出力値
 ${gpio_app} set ${port} op              # ポート番号portのGPIOを出力に設定
 d=("dl" "dh")                           # GPIOの論理値の定義
 while true; do                          # 永久ループ
-    # echo "GPIO"${port}"="${b}         # ポート番号と変数bの値を表示
+    echo "GPIO"${port}"="${b}           # ポート番号と変数bの値を表示
     ${gpio_app} "set" ${port} ${d[${b}]}  # GPIOに変数bの値を出力
     sleep 0.5                           # 0.5秒間の待ち時間処理
     b=$((!b))                           # 変数bの値を論理反転
