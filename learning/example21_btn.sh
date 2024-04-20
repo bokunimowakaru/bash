405803269067a631fdaac7f7a196b1363f741fdd
Wataru KUNINO <git@bokunimo.net>
2024-04-21 00:27:53 +0900

    ボタン動作の論理反転

--- learning/example21_btn.sh
+++ learning/example21_btn.sh
@@ -15,12 +15,8 @@ udp_port=1024                                           # CSVxUDP ポート番�
 device="btn_s_1"                                        # CSVxUDP デバイス名
 
 udp_app="../tools/udp_sender.py"                        # UDP送信プログラム
-gpio_app="pinctrl"                      # GPIO制御に新しいpinctrlを使用する
-# gpio_app="../gpio/gpio_zero.sh"       # GPIO制御に標準のgpio_zero.shを使用する
-# gpio_app="raspi-gpio"                 # GPIO制御に従来のraspi-gpioを使用する
-if [[ ${gpio_app} = "../gpio/gpio_zero.sh" ]]; then
-   trap "${gpio_app} quit" EXIT         # 終了時にGPIO用のHTTPサーバを停止する
-fi
+gpio_app="pinctrl"                          # GPIO制御にpinctrlを使用する
+# gpio_app="raspi-gpio"                     # GPIO制御にraspi-gpioを使用する
 
 # LINE送信用の関数line_notify()定義部
 line_notify() {                             # メッセージ送信用の関数
@@ -38,21 +34,32 @@ line_notify() {                             # メッセージ送信用の関数
 }
 
 # 主要処理部
-echo "Usage: "${0}                      # プログラム名と使い方を表示する
-port=4                                  # GPIO ポート番号
-${gpio_app} "set" ${port} "ip"          # ポート番号portのGPIOを入力に設定
-b=0                                     # 前回値を保持
+echo "Usage: "${0}                          # プログラム名と使い方を表示する
+port=26                                     # GPIO ポート番号
+${gpio_app} "set" ${port} "ip"              # ポート番号portのGPIOを入力に設定
+${gpio_app} "set" ${port} "pu"              # ポート番号portをプルアップ
+prev=0                                      # 前回値を保持する変数prev
 
-while true; do                          # 永久ループ
-    btn=`${gpio_app} get ${port}`       # ボタン状態を取得
-    val=`echo ${btn}|tr " " "\n"|grep "level="` # ボタンレベル値を取得
-    val=${val:6:1}                      # レベル値を抽出
-    if [[ ${val} && ${val} -ne ${b} ]]; then
-        b=${val}
-        echo ${device}","${b}|${udp_app} ${udp_port}  # CSVxUDP送信
-        if [[ ${b} = 1 ]]; then
-            line_notify "ボタンが押されました"
+while true; do                              # 永久ループ
+    btn=`${gpio_app} get ${port}`           # ボタン状態を取得
+    if [[ ${btn:15:2} = "lo" ]]; then       # 入力値がLレベルの時
+        val=0                               # 変数valに0を代入
+    elif [[ ${btn:15:2} = "hi" ]]; then     # 入力値がHレベルの時
+        val=1                               # 変数valに1を代入
+    else                                    # その他の場合(raspi-gpioなど)
+        val=`echo ${btn}|tr " " "\n"|grep "level="` # ボタンレベル値を取得
+        val=${val:6:1}                      # レベル値を抽出
+        if [[ ${val} = "" ]]; then          # レベル値が得られなかったとき
+            val=0                           # 変数valに0を代入
         fi
     fi
-    sleep 0.2                           # 0.1秒間の待ち時間処理
+    val=$(( ! ${val} ))                     # 変数valの論理を反転
+    if [[ ${val} -ne ${prev} ]]; then       # 変数valの値が前回と異なる時
+        echo ${device}","${val}|${udp_app} ${udp_port}  # CSVxUDP送信
+        if [[ ${val} == 1 ]]; then          # ボタンが押されていた時
+            line_notify "ボタンが押されました"
+        fi
+        prev=${val}                         # 変数valの値を変数prevに保持
+    fi
+    sleep 0.2                               # 0.2秒間の待ち時間処理
 done
