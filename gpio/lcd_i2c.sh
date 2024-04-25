38343568a4918c4999ea2e04a6323102b53d2a27
Wataru KUNINO <git@bokunimo.net>
2024-04-25 20:53:25 +0900

    空白で埋める

--- gpio/lcd_i2c.sh
+++ gpio/lcd_i2c.sh
@@ -2,35 +2,25 @@
 # I2C LCD
 
 ################################################################################
-# raspi_lcd_i2c.sh サンプル・ソフトウェア
+# raspi_lcd_i2c.sh サンプル・ソフトウェア for Raspberry Pi 5
 #
 #                                       Copyright (c) 2022 - 2024 Wataru KUNINO
 ################################################################################
+# 元ソース：
+# https://github.com/bokunimowakaru/raspi_lcd/blob/master/example.sh
+################################################################################
 # 解説：
 #   実行するとLCDに文字列を表示します。
-#
-# 要件：
-#   本ソフトのインストール($よりも後ろのコマンドを入力)：
-#   $ cd ⏎
-#   $ sudo apt install raspi-gpio ⏎ (古いOS使用時)
-#   $ sudo apt install git ⏎ (LITE版OS使用時)
-#   $ git clone https://bokunimo.net/git/raspi_lcd ⏎
-#   $ cd raspi_lcd ⏎
-#
-# (参考情報)LCD表示用プログラム raspi_lcd_i2c.sh の使い方：
-#   $ cd ~/raspi_lcd ⏎
-#   $ ./raspi_lcd_i2c.sh -h ⏎
 
-LCD_WIDTH=8                 # LCDの桁数 AQM0802は8桁、AQM1602は16桁
-LCD_IO=4                    # LCD用電源用IOポート番号を指定する
-LOG="/dev/stdout"           # ログファイル名(/dev/stdoutでコンソール表示)
+LCD_WIDTH=8                     # LCDの桁数 AQM0802は8桁、AQM1602は16桁
+LCD_IO=4                        # LCD用電源用IOポート番号を指定する
+LOG="/dev/stdout"               # ログファイル名(/dev/stdoutでコンソール表示)
 
-gpio_app="pinctrl"                      # GPIO制御にpinctrlを使用する
-# gpio_app="../gpio/gpio_zero.sh"       # GPIO制御にgpio_zero.shを使用する
-# gpio_app="raspi-gpio"                 # GPIO制御に従来のraspi-gpioを使用する
+gpio_app="pinctrl"              # GPIO制御にpinctrlを使用する for Raspberry Pi 5
+# gpio_app="raspi-gpio"         # GPIO制御に従来のraspi-gpioを使用する
 
-d=("dl" "dh")                           # GPIOの論理値の定義
-${gpio_app} set ${LCD_IO} op            # ポート番号portのGPIOを出力に設定
+d=("dl" "dh")                   # GPIOの論理値の定義
+${gpio_app} set ${LCD_IO} op    # ポート番号portのGPIOを出力に設定
 
 # 日時表示用 8桁/16桁LCD用
 date (){
@@ -75,6 +65,10 @@ lcd (){
     fi
     hex1=`echo -n ${s1}| iconv -f utf8 -t sjis | od -An -tx1 | sed 's/ / 0x/g'`
     hex2=`echo -n ${s2}| iconv -f utf8 -t sjis | od -An -tx1 | sed 's/ / 0x/g'`
+    hex1_0x20n=$(( ( ${LCD_WIDTH} * 5 - ${#hex1} ) / 5 ))
+    hex2_0x20n=$(( ( ${LCD_WIDTH} * 5 - ${#hex2} ) / 5 ))
+    hex1=${hex1}`printf %${hex1_0x20n}s | sed "s/ / 0x20/g"`
+    hex2=${hex2}`printf %${hex2_0x20n}s | sed "s/ / 0x20/g"`
     echo "hex1="${hex1} >> $LOG 2>&1
     echo "hex2="${hex2} >> $LOG 2>&1
     if [ -n "${s2}" ]; then
@@ -91,24 +85,24 @@ lcd (){
     echo `date` "LCD" ${s1} "/" ${s2} >> $LOG 2>&1
 }
 
-echo "Usage: "${0}                      # プログラム名と使い方を表示する
+echo "Usage: "${0}" [string1] [string1]"    # プログラム名と使い方を表示する
 
 # LCDの初期設定
 echo `date` "STARTED -------------------------------" >> $LOG 2>&1
 lcd_reset >> $LOG 2>&1
-lcd >> $LOG 2>&1
-sleep 5
+sleep 1
 
-# LCDへの文字列表示例
-
-echo `date` "Example for AQM1602A/Y/Grove ----------" >> $LOG 2>&1
-lcd "`date|cut -c1-16`" "AQM1602A/Y/Grove" >> $LOG 2>&1
-sleep 5
-lcd "0123456789ABCDEF" "16 ｹﾀ ﾏﾃﾞ ﾋｮｳｼﾞ!" >> $LOG 2>&1
-sleep 5
-
-echo `date` "Example for AQM0802A ------------------" >> $LOG 2>&1
-lcd "`date|cut -c1-16`" "AQM1602A/Y/Grove" >> $LOG 2>&1
-sleep 5
-lcd "01234567" "8ｹﾀﾋｮｳｼﾞ" >> $LOG 2>&1
-sleep 5
+# LCDへの文字列表示
+if [ "$#" -ge 1 ]; then
+    lcd ${1} ${2}
+else
+    echo `date` "Example for AQM1602A/Y/Grove ----------" >> $LOG 2>&1
+    lcd "`date|cut -c1-16`" "AQM1602A/Y/Grove" >> $LOG 2>&1
+    sleep 5
+    lcd "0123456789ABCDEF" "16 ｹﾀ ﾏﾃﾞ ﾋｮｳｼﾞ!" >> $LOG 2>&1
+    sleep 5
+    echo `date` "Example for AQM0802A ------------------" >> $LOG 2>&1
+    lcd "`date|cut -c1-16`" "AQM1602A/Y/Grove" >> $LOG 2>&1
+    sleep 5
+    lcd "01234567" "8ｹﾀﾋｮｳｼﾞ" >> $LOG 2>&1
+fi
