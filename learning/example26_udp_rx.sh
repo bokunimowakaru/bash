a60c9e623c72784afe2be96157dd6c0eb9a579c4
Wataru KUNINO <git@bokunimo.net>
2024-04-30 11:52:01 +0900

    インデント、コメントの

--- learning/example26_udp_rx.sh
+++ learning/example26_udp_rx.sh
@@ -7,14 +7,14 @@
 ################################################################################
 
 echo "Usage: "${0}" [udp port (1024~65535)]" # プログラム名と使い方を表示する
-PORT=1024                       # UDP待ち受けポート番号
+PORT=1024                                   # UDP待ち受けポート番号
 if [[ ${#} -ge 1 ]]; then
     PORT=$1
 fi
-echo "Listening UDP port "${PORT}"..."          # ポート番号表示
+echo "Listening UDP port "${PORT}"..."      # ポート番号表示
 
-while true; do                                  # 永久ループ
-    UDP=`nc -luw0 ${PORT}`                      # UDPパケットを取得
-    DATE=`date "+%Y/%m/%d %R"`                  # 日時を取得
-    echo -E $DATE, $UDP                         # 取得日時とデータを表示
-done                                            # 永久ループを繰り返す
+while true; do                              # 永久ループ
+    UDP=`nc -luw0 ${PORT}`                  # UDPパケットを取得
+    DATE=`date "+%Y/%m/%d %R"`              # 日時を取得
+    echo -E $DATE, $UDP                     # 取得日時とデータを表示
+done                                        # 永久ループを繰り返す
