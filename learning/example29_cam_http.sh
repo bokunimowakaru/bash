852363c91e5a6beb19cf0a27ae213c372087eefb
Wataru KUNINO <git@bokunimo.net>
2024-05-03 18:13:39 +0900

    訂正

--- learning/example29_cam_http.sh
+++ learning/example29_cam_http.sh
@@ -7,29 +7,41 @@
 IP=""                                   # 本機のIPアドレス
 PORT=8080                               # 待ち受けポート番号
 
-mkfifo payload
+mkfifo payload_tx                       # HTTPデータ送信用のパイプを作成
+trap "rm -f payload_tx || exit" SIGINT  # Ctrl-Cでパイプ切断し、プログラムを終了
 if [[ ${IP} = "" ]]; then IP=`hostname -I|cut -d" " -f1`; fi # IPアドレスを取得
-HTML_HOME="HTTP/1.0 200 OK\nContent-Type: text/html\nConnection: close\n\n\
-    <html>\n<head>\n<title>Camera</title>\n\
+URL="http://"${IP}":"${PORT}
+HTML="HTTP/1.0 200 OK\nContent-Type: text/html\nConnection: close\n\n<html>\n\
+    <head>\n<title>Camera</title>\n\
     <meta http-equiv=\"Content-type\" content=\"text/html; charset=UTF-8\">\
     \n</head>\n<body>\n<h3>Camera</h3>\n\
-    <form method=\"GET\" action=\"http://"${IP}":"${PORT_HTTP}"/\">\n\
-    Snap <input type=\"submit\" value=\"送信\">\n</form>\n</html>\n\n\
+    <p><form method=\"GET\" action=\""${URL}"/\">\n\
+    Snap <input type=\"submit\" value=\"送信\">\n</form></p>\
+    <p><a href=\""${URL}"/cam.jpg\">"${URL}"/cam.jpg</a></p></html>\n\n
 "                                       # HTTP + HTMLコンテンツ
-HTML_JPG="HTTP/1.0 200 OK\nContent-Type: image/jpeg\nConnection: close\n\n"
+head="HTTP/1.0 200 OK\nContent-Type: image/jpeg\nContent-Length: LENGTH\n\"
+error="HTTP/1.0 404 Not Found\n\n"
+
+# HTTPリクエスト受信と、リクエストに応じたレスポンスを出力
+payload_rx (){
+    while read tcp; do
+        HTTP=`echo -E ${tcp}|cut -d"=" -f1`             # HTTPコマンドを抽出
+        if [[ ${HTTP:0:13} = "GET /cam.jpg " ]]; then
+            LENGTH=`ls -l cam.jpg|cut -d" " -f5`
+            echo -e ${head}|sed -e "s/LENGTH/${LENGTH}/g" > head.http
+            cat head.http cam.jpg 
+        elif [[ ${HTTP:0:6} = "GET / " ||  ${HTTP:0:7} = "GET /? " ]]; then
+            echo -e ${HTML} 
+            raspistill -n -o cam.jpg --width 320 --height 240
+        elif [[ ${HTTP:0:5} = "GET /" ]]; then
+            echo -e ${error}
+            raspistill -n -o cam.jpg --width 320 --height 240
+        fi
+    done
+}
 
 # メイン処理部 #################################################################
-echo "HTTP Server Started http://"${IP}":"${PORT}"/"    # アクセス用URL表示
-while true; do                                          # HTTP待ち受け
-    nc -lw1 -v 8080 < payload\
-    |while read tcp; do
-        DATE=`date "+%Y/%m/%d %R"`                      # 時刻を取得
-        HTTP=`echo -E ${tcp}|cut -d"=" -f1`             # HTTPコマンドを抽出
-        if [[ ${HTTP} = "GET /?" || ${HTTP} = "GET /?SNAP" ]]; then
-            echo ${HTML_HOME} > payload
-            libcamera-jpeg -o cam.jpg --width 320 --height 240
-        elif [[ ${HTTP:0:13} = "GET /cam.jpg " ]]; then
-            echo ${HTML_JPG} > payload
-            cat cam.jpg >> payload
-        fi
-done; done                                              # 繰り返しここまで
+while true; do                                              # 繰り返し処理
+    echo "HTTP Server Started http://"${IP}":"${PORT}"/"    # アクセス用URL表示
+    nc -lw1 -v 8080 < payload_tx| payload_rx > payload_tx   # HTTP待ち受け
+done                                                        # 繰り返しここまで
